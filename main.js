773cd09bacf1ae233f7613452248fd450a72d695
Automoroboto <roberto@studiodespirito.co.uk>
2023-03-17 23:28:00 +0000

    added comments

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ var result = document.getElementById("result");
 var compy = document.getElementById("compy");
 
 
+//below is code which uses an event listener to communicate with the buttons in the html with the 
+//relevant tags. in the body of the event listener function the play function is given the relevant player choice argument.
+// style.background colour is also triggered when button is clicked to change the color of the button to indicate a choice.
 document.getElementById("rock"). addEventListener("click", function() {
 	play("rock");style.backgroundColor;
 });
@@ -20,8 +23,11 @@ document.getElementById("scissors").addEventListener("click", function() {
 
 function play(playerChoice) {
 	
+	// the code below assigns an array to the variable computerChoice. The array calls a random value each time.
 	let computerChoice = choices[Math.floor(Math.random() * choices.length)];
 	
+
+	//The code below is the main logic for 'rock paper scissors' where the result of each possibility is communicated to the <p></p> element with the id="result"
 	if (playerChoice === computerChoice) {
 		result.innerHTML = "Man and Machine are as one!";
 	} else if ((playerChoice === "rock" && computerChoice === "scissors") || 
@@ -32,6 +38,8 @@ function play(playerChoice) {
 		result.innerHTML = "How could you let a machine beat you?!! YOU FILTHY APE!!";
 	}
 	
+	//code below creates a message in the html index indicating computer choice 
+	//compy.innerHTML communicates the string to the <p></p> code in the html file with the id="compy"
 	if (computerChoice === "rock"){
 		compy.innerHTML = "Computer picks Rock!"
 	}else if (computerChoice === "paper") {
